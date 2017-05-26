537f964209d70dc2b38b7982e4c0fbdbf5f6f831
Raman Singh <raman.singh@dgbes.com>
2017-05-26 10:06:15 +0530

    Reverted some restructuring of CMakeLists.txt file

--- cmake/policies.cmake
+++ cmake/policies.cmake
@@ -2,5 +2,4 @@ if (CMAKE_MAJOR_VERSION GREATER 2)
     cmake_policy(SET CMP0022 OLD) # interface link libraries
     cmake_policy(SET CMP0042 NEW) # osx rpath
     cmake_policy(SET CMP0011 NEW) # policy setting
-    cmake_policy(SET CMP0048 NEW) # policy setting
 endif()
